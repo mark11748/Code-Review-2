dfc1488493aff77d1aea06605d1e81919e467cb3
Mark Woodward <markwood117@gmail.com>
2017-06-14 16:07:21 -0700

    add basic functionality. NEEDS: polishing, additional question and possibly more elaborate css formatting.

---
+++ js/script1.js
@@ -0,0 +1,41 @@
+//var name  = "ERR" ;
+//var title = "ERR" ;
+//var age   = "ERR";
+
+$(document).ready(function() {
+  $('#SCREEN-1').show();
+  $('#SCREEN-2').hide();
+  $('#SCREEN-3').hide();
+
+  $('#SCREEN-1 form').submit(function (event){
+    $('#SCREEN-1').toggle();
+    $('#SCREEN-2').toggle();
+
+    var name  = $("input#name").val();
+    var title = $("input#title").val();
+    var age   = $("input#age").val();
+
+    $('span#name').empty().append(name);
+    $('span#title').empty().append(title);
+
+    event.preventDefault();
+  });
+
+  $('#SCREEN-2 form').submit(function (event){
+    $('#SCREEN-2').toggle();
+    $('#SCREEN-3').toggle();
+
+    var area         = $("select#area").val();
+    var participants = $("select#participants").val();
+    var tripType     = $("select#tripType").val();
+    var price        = $("input#price").val();
+
+
+    $('span#area').empty().append(area);
+    $('span#tripType').empty().append(tripType);
+    $('span#price').empty().append("$"+(parseInt(price)*.8).toString());
+
+
+    event.preventDefault();
+  });
+});
